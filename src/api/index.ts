d5c1a034fea98935ed4770fc8fdd02bdac3124e2
villainhr <villainthr@gmail.com>
2020-02-14 14:02:54 +0800

    feat: update

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,2 @@
 export * from "../interaction/index"
+export * from "../network/request"
--- src/network/request.ts
+++ src/network/request.ts
@@ -1,19 +1,115 @@
-import jsonpRetry from 'jsonp-retry'
-import 'whatwg-fetch'
+import { fetch } from "whatwg-fetch"
+import { validateUrl, getType, addQueryStringToUrl, convertObjectValueToString, urlEncodeFormData } from "../utils/index"
+import {AnyObj} from '../api/index.d'
 
-const params = {
-    method: "GET",
-    dataType: "json",
-    responseType: "text",
-    header: {
-        "content-type": "application/json"
-    },
-    success: () => {},
-    fail: () => {},
-    complete: () => {},
-}
+class RequestTask {
+    params = {
+        url: "",
+        method: "GET",
+        dataType: "json",
+        responseType: "text",
+        header: {
+            "content-type": "application/json"
+        },
+        success: () => { },
+        fail: () => { },
+        complete: () => { },
+    }
 
-export function request(options = {}) {
-    
+    requestHander: Promise<any>
 
+    constructor(params = {}) {
+        const options = Object.assign(
+            {}, this.params, params
+        )
+
+    }
+    request(options) {
+        let url = options.url
+        const method = options.method.toUpperCase()
+
+        if (validateUrl(url) === false) {
+            console.error("request", options, `invalid url "${url}"`)
+            return undefined
+        }
+
+        // 检查 header
+        let headers = convertObjectValueToString(options.header)
+        headers = Object.keys(headers).reduce((pre: { [key in string]: string }, cur: string) => {
+            if (cur.toLowerCase() === 'content-type') {
+                pre[cur.toLowerCase()] = headers[cur]
+            } else {
+                pre[cur] = headers[cur]
+            }
+            return pre
+        }, {})
+
+        let responseType = "text"
+        if (options.responseType) {
+            responseType = options.responseType.toLowerCase()
+        }
+
+        let body = options.data
+        if (method === "GET" || method === "HEAD") {
+            url = addQueryStringToUrl(url, body)
+        } else if (typeof body !== "string" && getType(body) !== "ArrayBuffer") {
+            if (headers["content-type"].indexOf("application/x-www-form-urlencoded") > -1) {
+                body = urlEncodeFormData(body, true)
+            } else if (headers["content-type"].indexOf("application/json") > -1 || typeof body === "object") {
+                body = JSON.stringify(body)
+            } else {
+                body = body.toString()
+            }
+        }
+
+        let result: AnyObj = {}
+        return fetch(url, {
+            method,
+            headers,
+            body
+        })
+            .then( (response: Response) => {
+                result.statusCode = response.status
+                result.header = {}
+                response.headers.forEach((val: string, key: string) => {
+                    result.header[key] = val
+                  })
+                
+                if (response.ok === false) {
+                    throw response
+                }
+
+                if (responseType === "arraybuffer") {
+                    return response.arrayBuffer()
+                }
+
+                if (options.dataType === "json") {
+                    return response.json()
+                }
+
+                if(responseType === "text") {
+                    return response.text()
+                }
+
+                return Promise.resolve()
+            })
+            .then( (data: string | ArrayBuffer | unknown) => {
+                result.data = data
+                options.success(result)
+                options.complete(result)
+                
+            })
+    }
+
+    then(Resolver, Rejecter) {
+
+    }
+
+    catch(Rejecter) {
+
+    }
+
+    abort() {
+
+    }
 }
